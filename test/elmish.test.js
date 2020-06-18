6a6a87e20c368e12ee8a57ee418be86eb2066a32
Scott <Scotthall1983@hotmail.com>
2020-06-18 19:42:42 +0100

    Added notes to explain details of all the require statements.

--- test/elmish.test.js
+++ test/elmish.test.js
@@ -1,9 +1,9 @@
 const test = require("tape");
-const fs = require("fs");
-const path = require("path");
+const fs = require("fs"); // this allows reading of html files
+const path = require("path"); // so you can open files cross platform
 const html = fs.readFileSync(
   path.resolve(__dirname, "../examples/todo-list/index.html")
-);
+); //sample html file to initialise JSDOM
 require("jsdom-global")(html);
-const elmish = require("../examples/todo-list/elmish.js");
-const id = "test-app";
+const elmish = require("../examples/todo-list/elmish.js"); // the functions we want to test
+const id = "test-app"; // all tests use 'test-app' as root element
