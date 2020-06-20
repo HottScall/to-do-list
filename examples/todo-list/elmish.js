7f515d80d24954f6de88b30f980c53b1944e26f6
Scott <Scotthall1983@hotmail.com>
2020-06-20 23:11:56 +0100

    Added JS Doc details to ensure user knows what the function should achieve.

--- examples/todo-list/elmish.js
+++ examples/todo-list/elmish.js
@@ -1,5 +1,8 @@
 /**
  * `empty` deletes all the DOM elements from within a specific "root element"
  * it is used to erase the DOM before re-rendering the app
- *
+ * @param {Object} node the exact ("parent") DOM node you want to empty
+ * @example
+ * // returns true once the 'app' node is emptied
+ * empty (document.getElementById('app'))
  */
