0ac176181a0146aef5363d2c38c24e0b5cc5cf41
Scott <Scotthall1983@hotmail.com>
2020-06-20 23:07:02 +0100

    Corrected directories to ensure the fs.readFileSync works and changed an alreay used variable from text to txt.

--- test/elmish.test.js
+++ test/elmish.test.js
@@ -1,9 +1,7 @@
 const test = require("tape");
 const fs = require("fs"); // this allows reading of html files
 const path = require("path"); // so you can open files cross platform
-const html = fs.readFileSync(
-  path.resolve(__dirname, "../examples/todo-list/index.html")
-); //sample html file to initialise JSDOM
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html")); //sample html file to initialise JSDOM
 require("jsdom-global")(html);
 const elmish = require("../examples/todo-list/elmish.js"); // the functions we want to test
 const id = "test-app"; // all tests use 'test-app' as root element
@@ -14,7 +12,7 @@ test('empty "root" removes DOM elements from container', function(t) {
   const root = document.getElementById(id);
   const div = document.createElement("div");
   div.id = "mydiv";
-  const text = document.createTextNode(text);
+  const txt = document.createTextNode(text);
   div.appendChild(txt);
   div.appendChild(div);
   // now check the text of the div
