ba61366566bcbb02019b45e9d61a879c1632df2e
Scott <Scotthall1983@hotmail.com>
2020-06-20 23:08:38 +0100

    Fixed test to ensure correct error message returned.

--- test/elmish.test.js
+++ test/elmish.test.js
@@ -6,21 +6,21 @@ require("jsdom-global")(html);
 const elmish = require("../examples/todo-list/elmish.js"); // the functions we want to test
 const id = "test-app"; // all tests use 'test-app' as root element
 
-test('empty "root" removes DOM elements from container', function(t) {
-  // set up the test div
-  const text = "hello world";
+test('empty("root") removes DOM elements from container', function(t) {
+  // setup the test div:
+  const text = "Hello World!";
   const root = document.getElementById(id);
   const div = document.createElement("div");
   div.id = "mydiv";
   const txt = document.createTextNode(text);
   div.appendChild(txt);
-  div.appendChild(div);
-  // now check the text of the div
-  const actual = document.getElementById("mydiv").textContext;
-  t.equal(actual, text, "contents of my div is:" + actual + "==" + text);
+  root.appendChild(div);
+  // check text of the div:
+  const actual = document.getElementById("mydiv").textContent;
+  t.equal(actual, text, "Contents of mydiv is: " + actual + " == " + text);
   t.equal(root.childElementCount, 1, "Root element " + id + " has 1 child el");
-  // empty the DOM node
-  elmish.empty(root);
-  t.equal(root.childElementCount, 0, "After empty(root) has 0 child elements");
+  // empty the root DOM node:
+  elmish.empty(root); // <-- exercise the `empty` function!
+  t.equal(root.childElementCount, 0, "After empty(root) has 0 child elements!");
   t.end();
 });
